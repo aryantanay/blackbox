606fce7486b7f5a3a3c9311045cb0e354fbc4af7
WraitH17 <ss0087@srmist.edu.in>
2023-02-19 20:52:48 +0530

    :iphone: fix mobile view

--- client/components/shared/fileInput.tsx
+++ client/components/shared/fileInput.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import { TbFileUpload } from 'react-icons/tb';
-import storageService from 'utils/services/storage';
+import { useState } from "react";
+import { TbFileUpload } from "react-icons/tb";
+import storageService from "utils/services/storage";
 
 const FileInput = () => {
   const files: File[] = [];
-  const [file, setFile] = useState('');
+  const [file, setFile] = useState("");
 
   const addFile = async ({ target }: any) => {
     const inputFiles: File = target.files[0];
@@ -16,11 +16,12 @@ const FileInput = () => {
 
   return (
     <div className="flex flex-col items-center justify-center w-full text-3xl">
-      <p className="txt-3xl"> Web3-Storage</p>
-      {file ? <p className="text-2xl pb-10">{file}</p> : ''}
+      <p className="text-5xl md:text-7xl mt-6 md:mt-0"> Blackbox</p>
+      <p className="text-2xl md:text-4xl mb-16 mt-3"> Web3-Storage</p>
+      {file ? <p className="text-2xl pb-10">{file}</p> : ""}
       <label
         htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-2/12 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        className="flex flex-col items-center justify-center md:w-2/12 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
       >
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <div className="h-16 w-16">
@@ -41,11 +42,11 @@ const FileInput = () => {
           onChange={addFile}
         />
       </label>
-      <p className="text-2xl flex flex-col pt-10">
+      <p className="text-xl flex flex-col items-center pt-10 md:mt-32 mt-6">
         <div>Made By :</div> <div>Sarthak Sachdeva</div>
         <div>Mehul Kumar</div>
         <div>Saransh Sinha</div>
-        <div>Aryan tanay</div>
+        <div>Aryan Tanay</div>
       </p>
     </div>
   );
